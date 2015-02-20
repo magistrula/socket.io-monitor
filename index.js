53f2186828b82776468698cd962687d38ee7d518
Anna Andresian <aandresian@amplify.com>
2015-02-20 11:03:59 -0500

    Remove commented code

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ var io = socketio(srv, {
 });
 
 srv.listen(5000, function(){
-  console.log(arguments);
   console.log('Socket.io server listening on port 5000');
 });
 
@@ -18,31 +17,17 @@ io.use(monitor);
 io.on('connect', function(socket){
   console.log('Socket connected');
 
-  // socket.onpacket = function(){
-  //   console.log('socket: packet:', arguments);
-  // }
-
-  // socket.on('event', function(){
-  //   console.log('socket: event:', arguments);
-  // })
-
   socket.on('disconnect', function(socket){
     console.log('Socket disconnected');
   });
 
   socket.on('pizza_channel', function(data){
     io.emit('pizza_channel', data);
-    // console.log('Received payload:', data);
   });
 
   socket.on('taco_channel', function(data){
     io.emit('taco_channel', data);
-    // console.log('Received payload:', data);
   });
 
-  socket.on('*', function(packet){
-    // count++;
-    // console.log('message:', packet);
-  });
 });
 
--- monitor.js
+++ monitor.js
@@ -26,10 +26,6 @@ srv.listen(5001, function(){
 
 messageCounter = new MessageCounter();
 
-setInterval(function(){
-  // console.log(messageCounter.data);
-}, 1000);
-
 var broadcastSocketEvent = function(socketId, type) {
   io.to('messages').emit(type + '_received', {socketId: socketId, date: new Date()});
 }
@@ -44,16 +40,7 @@ var decodePacketEvent = function(packet, callback){
   decoder.destroy();
 }
 
-var hostIo;
-
 module.exports = function(socket, next){
-  // if(!hostIo){
-  //   hostIo = socket.nsp;
-  //   hostIo.on('connect', function(){
-  //     broadcastSocketEvent(socket.id, 'connected');
-  //   });
-  // }
-
   socket.on('disconnect', function(){
     broadcastSocketEvent(socket.id, 'disconnected');
   });
@@ -68,7 +55,6 @@ module.exports = function(socket, next){
   });
 
   socket.conn.on('packet', function(packet){
-    console.log('ping', socket.id);
     if (packet.type === 'ping'){
       broadcastSocketEvent(socket.id, packet.type);
     }
